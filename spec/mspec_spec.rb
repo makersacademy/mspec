07da60144c5eb2adfb0c9268cc4f82de16bdb30b
mdsomerfield <mark.somerfield@makersacademy.com>
2018-08-06 13:53:43 +0100

    Sanitised mspec.it test and associated logs

--- spec/mspec_spec.rb
+++ spec/mspec_spec.rb
@@ -4,20 +4,25 @@ test_runner = MSpec.new
 
 p "Starting tests"
 
-test_runner.it "Stores the test in the tests hash with the description" do
+test_runner.it "Runs all tests passed in through the 'it' method" do
   # arrange
   subject = MSpec.new
   test_var = 0;
+
   subject.it "Test description" do
-    test_var = 1
+    test_var += 1
+  end
+
+  subject.it "Test description" do
+    test_var += 2
   end
 
   # act
   subject.run
 
   # assert
-  if test_var != 1
-    raise "Number of tests is not one"
+  if test_var != 3
+    raise "Not all tests have been run"
   else
     p "Test passed"
   end
